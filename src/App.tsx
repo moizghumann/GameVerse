a4051b7f5a9f5241f950857716f8e32ed55ad5f1
moizghumann <moizghuman@gmail.com>
2023-06-07 00:16:07 +0500

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 const App = () => {
   return (
     // "" is a row
     // "a b c " a b c are coulmns
-    <Grid templateAreas={`"nav nav" "aside main"`} gap={1}>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,           // for mobile
+      lg: `"nav nav" "aside main"`    // for 1280px viwpoint and higher
+    }} gap={1}>
       <GridItem area='nav' bg='yellow'>
-        Yellow Box
+        nav
       </GridItem>
-      <GridItem area='aside' bg='coral'>
-        Red Box
-      </GridItem>
+      {/* s The Show component controls the visibility of its children based on the screen size */}
+      <Show above='lg'>
+        <GridItem area='aside' bg='coral'>
+          aside
+        </GridItem>
+      </Show>
       <GridItem area='main' bg='dodgerblue'>
-        Red Box
+        main
       </GridItem>
     </Grid>
   )
