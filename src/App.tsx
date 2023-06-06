41742a74d29b475d5391e11b86741c62488df848
moizghumann <moizghuman@gmail.com>
2023-06-06 23:50:44 +0500

    grid in chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 const App = () => {
-  return <Button colorScheme='blue'>Button</Button>
+  return (
+    // "" is a row
+    // "a b c " a b c are coulmns
+    <Grid templateAreas={`"nav nav" "aside main"`} gap={1}>
+      <GridItem area='nav' bg='yellow'>
+        Yellow Box
+      </GridItem>
+      <GridItem area='aside' bg='coral'>
+        Red Box
+      </GridItem>
+      <GridItem area='main' bg='dodgerblue'>
+        Red Box
+      </GridItem>
+    </Grid>
+  )
 }
 
 export default App
