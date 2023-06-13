0270293591dfffd23e8aea7885f20b5188fb8ab0
moizghumann <moizghuman@gmail.com>
2023-06-14 00:10:13 +0500

    refactoring game grid component

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App = () => {
       </GridItem>
       {/* s The Show component controls the visibility of its children based on the screen size */}
       <Show above='lg'>
-        <GridItem area='aside' bg='#242C39' borderRadius={10} paddingLeft={4}>
+        <GridItem area='aside' borderRadius={10} paddingLeft={4}>
           {/* getting data as prop from GenreSideBar to pass it as a prop to GameGrid to show only the selected GameCards by configuring the api request */}
           <GenreSideBar selectedGenre={gameQuery.genre} onSelectedGenre={(genre) => setGameQuery({ ...gameQuery, genre })} />
         </GridItem>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,26 +12,29 @@ const GameGrid = (gameQuery: GameQuery) => {
     const { data, error, loading } = useGames(gameQuery);
 
     // dummy values for the number of skeletons we want to show during loading
-    const skeletons = [1, 2, 3, 4, 5, 6]
+    const skeletons = [1, 2, 3, 4, 5, 6];
+
+    if (error) return <Text>{error}</Text>;
+
+    {/* SimpleGrid interface is used to make grid layouts */ }
     return (
-        <>
-            {error && <Text>{error}</Text>}
-            {/* SimpleGrid interface is used to make grid layouts */}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap={5} padding={5}>
-                {loading && skeletons.map(skeleton =>
-                    <GameCardContainer key={skeleton} >
-                        <GameCardSkeleton />
-                    </GameCardContainer>)}
+
+        <SimpleGrid
+            columns={{ sm: 1, md: 2, lg: 3, xl: 4 }
+            } gap={5} padding={5} >
+            {loading && skeletons.map(skeleton =>
+                <GameCardContainer key={skeleton} >
+                    <GameCardSkeleton />
+                </GameCardContainer>)}
 
 
-                {data?.map(game =>
+            {
+                data?.map(game =>
                     <GameCardContainer key={game.id} >
                         <GameCard games={game} />
-                    </GameCardContainer>)}
-
-            </SimpleGrid>
-            {console.log(data)}
-        </>
+                    </GameCardContainer>)
+            }
+        </SimpleGrid>
     )
 }
 
