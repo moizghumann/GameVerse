7d2f5e6f22d3029e71d758c047b5f21ac981b1ac
moizghumann <moizghuman@gmail.com>
2023-07-05 19:03:22 +0500

    dynamic game heading - fixed results for search

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -8,11 +8,22 @@ const GameHeading = () => {
 
   const genreID = useGameQueryStore(s => s.gameQuery.genreID)
   const platformID = useGameQueryStore(s => s.gameQuery.platformID)
+  const search = useGameQueryStore(s => s.gameQuery.search);
 
   const genres = useGenre(genreID)
   const platforms = usePlatform(platformID);
 
-  const heading = `${platforms?.name || ''} ${genres?.name || ''} Games`
+  // const heading = `${platforms?.name || ''} ${genres?.name || ''} Games` || `${search || ''} Search Results`
+
+  let heading = '';
+
+  if ((platforms?.name) || (genres?.name)) {
+    heading = `${platforms?.name || ''} ${genres?.name || ''} Games`;
+  } else if (search) {
+    heading = `search results for "${search}"`;
+  } else {
+    heading = 'Games';
+  }
 
   return (
     <Heading as={'h1'} fontSize={'50px'} textTransform={'uppercase'} marginBottom={4}>{heading}</Heading>
