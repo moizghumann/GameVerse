223955aeaaa067890fdd09acb6bd95027ae34ec2
moizghumann <moizghuman@gmail.com>
2023-07-05 19:04:01 +0500

    added navigation functionality to each game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import getCroppedGameImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
 import MetaCritic from "./MetaCritic";
 import PlatformsIconList from "./PlatformsIconList";
+import { Link } from "react-router-dom";
 
 // importing Games interface in our own interface here
 // this makes Cards access all the properties of Games
@@ -16,23 +17,22 @@ const GameCard = ({ games }: Prop) => {
     return (
         // overflow -> css property -> used to control how content that exceeds the size of its container is displayed
         <DarkMode>
-
-            <Card>
-                <Image src={getCroppedGameImageUrl(games.background_image)} />
-                <CardBody paddingTop={3} paddingBottom={5}>
-                    <HStack marginY={1.5} justifyContent={'space-between'} >
-                        {/* always name properties in props acc to data properties in api */}
-                        <PlatformsIconList platforms={games.parent_platforms.map(p => p.platform)} />
-                        <MetaCritic critic={games.metacritic} />
-                    </HStack>
-                    <Heading fontSize={"3xl"} marginTop={0.5}>
-                        {games.name}
-                        <Emoji rating={games.rating_top} />
-                    </Heading>
-
-                </CardBody>
-            </Card>
-
+            <Link to={'/games/:id'}>
+                <Card>
+                    <Image src={getCroppedGameImageUrl(games.background_image)} />
+                    <CardBody paddingTop={3} paddingBottom={5}>
+                        <HStack marginY={1.5} justifyContent={'space-between'} >
+                            {/* always name properties in props acc to data properties in api */}
+                            <PlatformsIconList platforms={games.parent_platforms.map(p => p.platform)} />
+                            <MetaCritic critic={games.metacritic} />
+                        </HStack>
+                        <Heading fontSize={"3xl"} marginTop={0.5}>
+                            {games.name}
+                            <Emoji rating={games.rating_top} />
+                        </Heading>
+                    </CardBody>
+                </Card>
+            </Link>
         </DarkMode>
     )
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,18 +2,21 @@ import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import useGameQueryStore from '../state-management/gameQueryStore';
+import { useNavigate } from 'react-router-dom';
 
 
 const SearchBar = () => {
 
     const ref = useRef<HTMLInputElement>(null)
     const updateSearch = useGameQueryStore(s => s.updateSearch)
+    const navigate = useNavigate();
 
     return (
         <>
             <form onSubmit={(event) => {
                 event.preventDefault();
                 if (ref.current) updateSearch(ref.current.value);
+                navigate('/')
             }} >
                 <InputGroup >
                     <InputLeftElement pointerEvents='none' paddingLeft={2} paddingTop={'0.9px'}>
